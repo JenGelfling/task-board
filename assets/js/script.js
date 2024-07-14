c184eb166b050c0fab94c78bb6f6809689b9f338
Ben Merchant <benmerch@gmail.com>
2024-07-14 18:15:56 -0500

    wrote createTaskCard function.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,54 @@
 // Retrieve tasks and nextId from localStorage
-let taskList = JSON.parse(localStorage.getItem("tasks"));
-let nextId = JSON.parse(localStorage.getItem("nextId"));
+let taskList = JSON.parse(localStorage.getItem("tasks")) || [];
+let nextId = JSON.parse(localStorage.getItem("nextId")) || [];
+const submitBtn = document.querySelector ("#submit-button");
+let taskId = 
+
+function readProjectsFromStorage(tasks){
+    // taskList = localStorage.getItem("tasks");
+    // localStorage.getItem(nextId);
+
+    return JSON.parse(localStorage.getItem("tasks")) || []
+}
+
+function saveProjectToStorage(tasks){
+    localStorage.setItem('tasks', JSON.stringify(taskList));
+    console.log(task);
+}
 
 // Todo: create a function to generate a unique task id
-function generateTaskId() {
+function generateTaskId(e) {
+    e.preventDefault();
+
 
 }
 
 // Todo: create a function to create a task card
-function createTaskCard(task) {
+function createTaskCard(taskOb) {
+    const cardBlock = document.createElement("div");
+    const h2Tag = document.createElement("h2");
+    const dueDate = document.createElement("p");
+    const contentTag = document.createElement("p");
 
+    h2Tag.textContent = titleInput.value;
+    dueDate.textContent = dateInput.value;
+    contentTag.textContent = contentInput.value;
+
+    cardBlock.appendChild(h2Tag);
+    cardBlock.appendChild(dueDate);
+    cardBlock.appendChild(contentTag);
+
+    if (taskOb.status === 'to-do'){
+        toDoContainer.appendChild(cardBlock)}
+        else if (taskOb.status === 'in-progress'){
+        toDoContainer.appendChild(cardBlock)}
+        else {
+        toDoContainer.appendChild(cardBlock)}
 }
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
+    existingTasks
 
 }
 
